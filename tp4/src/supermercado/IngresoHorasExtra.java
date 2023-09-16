4b81061823c9c3345ead3970337026bc5303a2c9
fabricio camilla <fachucamilla32@gmail.com>
2023-09-16 20:45:47 -0300

    update

---
+++ tp4/src/supermercado/IngresoHorasExtra.java
@@ -0,0 +1,21 @@
+package supermercado;
+
+public class IngresoHorasExtra extends Ingreso{
+
+    private int horasExtra;
+
+    public IngresoHorasExtra(int mesPercepcion, String concepto, double montoPercibido, int horasExtra) {
+        super(mesPercepcion, concepto, montoPercibido);
+        this.horasExtra = horasExtra;
+    }
+
+    public int getHorasExtra() {
+        return horasExtra;
+    }
+
+    @Override
+    public double getMontoImponible() {
+        return 0;
+    }
+
+}
