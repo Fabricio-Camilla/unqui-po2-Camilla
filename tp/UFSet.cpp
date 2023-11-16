2dcb05930c13a3348f99b7e1f9b17df1394f43b0
Fabricio Camilla <142159089+Fabricio-Camilla@users.noreply.github.com>
2023-11-15 23:09:17 -0300

    Add files via upload

--- tp/UFSet.cpp
+++ tp/UFSet.cpp
@@ -14,9 +14,17 @@
  */
 struct UFNode {
    ELEM_TYPE element;
-   UFSet parent;
+   UFNode* parent;
    int rank;
 };
+/*
+   INV. REP
+   * Los elementos son disjuntos entonces no pueden ser iguales.
+   * El rank es diferente a la altura.
+   * Si el UFSet tiene un solo elemento ese es su parent.
+   * Todo UFSet tiene un elemento distinguido.
+*/
+
 
 /* 
  * Inicializa el UFSet ufset, cuyo valor asociado será value 
@@ -24,7 +32,7 @@ struct UFNode {
 UFSet createUFS(ELEM_TYPE value) {
    UFSet set = new UFNode;
    set -> element = value;
-   set -> parent = .....;
+   set -> parent = set;
    set -> rank = 0;
    return set;
 }
@@ -37,22 +45,55 @@ ELEM_TYPE elemUFS(UFSet ufset) {
  * Encuentra el elemento distinguido para el UFSet dado. 
  * Esta operación puede ser optimizada con la técnica de compresión de camino.
  */
+
+
 UFSet findUFS(UFSet elem) {
-   UFSet padre = elem;
-   while( elem -> element != elem -> parent){
-      padre = elem -> parent;
+   //  *sin optimizacion*
+  /* while( elem != elem -> parent){
+      elem = elem -> parent;
    }
-   return padre;
+   return elem;
+*/
+      
+   UFSet current = elem;
+   while( current != current -> parent){
+      current = current -> parent;              // current es el elemnto distinguido
+   } 
+   while( current != elem -> parent){         // currrente no sea el distinguido
+         UFSet prox = elem -> parent;
+         elem -> parent = current;      //actualizo el parent del actual por el distinguido
+         elem = prox;               // actualizo el actual para el siguiente        
+      }
+   return current;
 }
+
+
+
 
 /*
  * Calcula la unión entre los conjuntos ufset1 y ufset2. 
  * Esta operación puede ser optimizada con la técnica de unión por rango.
  */
 void unionUFS(UFSet ufset1, UFSet ufset2) {
-   if(ufset1 -> parent < ufset2 -> parent){
-      ufset2-> parent = ufset1 -> parent;
-   }else if(ufset1 -> parent == ufset2 -> parent){
-      ufset2 -> parent = ufset1 -> parent;
+   /* 
+   //*sin optimizacion*
+   if(findUFS(ufset1)  < findUFS(ufset2)){
+      ufset2-> parent = findUFS(ufset1);
+   }else if(findUFS(ufset1) == findUFS(ufset2)){
+      ufset2 -> parent = findUFS(ufset1);
+   }
+   */
+
+   if (findUFS(ufset1) -> rank > findUFS(ufset2) -> rank){
+      ufset2 -> parent = findUFS(ufset1); 
+   }else if( findUFS(ufset1) -> rank < findUFS(ufset2) -> rank ){
+      ufset1 -> parent = findUFS(ufset2);     
+   }else{
+      ufset1 -> parent = findUFS(ufset2);
+      ufset2 -> rank ++;
    }
 }
+
+
+
+
